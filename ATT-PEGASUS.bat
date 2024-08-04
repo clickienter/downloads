35d37097c748dfcbf36b36d3ecc4017bb5fec846
ClickEnter <38695855+clickienter@users.noreply.github.com>
2024-08-04 01:15:24 -0300

    Delete ATT-PEGASUS.bat

--- ATT-PEGASUS.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-taskkill /F /IM PegasusClientPrism.exe
-net use w: \\192.168.0.8\storage\CPD\PEGASUS
-Copy w:\pegasus.exe C:\Softsystem
-net use w: /delete
-C:\Softsystem\pegasus.exe /verysilent
-C:\Softsystem\SoftPegasusClient\PegasusClientPrism.exe
